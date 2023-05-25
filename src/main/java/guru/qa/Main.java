01e76f64e0bc823bf2c828b1f82094e23426f60d
Nastya <kolod.02@mail.ru>
2023-05-26 02:58:58 +0700

    change

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -2,9 +2,8 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         String message = "Hello qa.guru!";
-        if (args.length ==0) {
-            System.out.println(message);
-
+        if (args.length == 0) {
+            System.out.println(message + "!!");
         }
     }
 }
